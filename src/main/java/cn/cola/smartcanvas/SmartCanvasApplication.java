b8f5328d586dd6c52f1c84ad89637d1ac27488db
ColaBlack <2698360477@qq.com>
2024-11-17 13:17:00 +0800

    init

---
+++ src/main/java/cn/cola/smartcanvas/SmartCanvasApplication.java
@@ -0,0 +1,18 @@
+package cn.cola.smartcanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动主类
+ *
+ * @author ColaBlack
+ */
+@SpringBootApplication
+public class SmartCanvasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCanvasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cola/smartcanvas/SmartCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cola.smartcanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCanvasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
